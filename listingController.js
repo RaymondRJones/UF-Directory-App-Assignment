b3efc4b0af87221b455fe6e9fc8e5473a23efe17
Raymond Jones <Rayjones2170@gmail.com>
2018-09-10 21:43:00 -0400

    Completes Controller functions

--- listingController.js
+++ listingController.js
@@ -1,14 +1,51 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
+    $scope.items = Listings;
     $scope.detailedInfo = undefined;
-
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    $scope.itemIndex = {value: -1};
+    $scope.itemCode = {value: ""};
+    $scope.itemCoordinateX;
+    $scope.itemCoordinateY;
+    $scope.test = {value: "empty"};
+    $scope.address_;
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function(code_, name_, latitude_,longitude_,address_) {
+      var obj = {
+        "code": code_,
+        "name": name_,
+        "coordinates": {
+          "latitude": latitude_,
+          "longitude": longitude_,
+        },
+        "address": address_
+      };
+      $scope.listings.push(obj);
+    };
+    $scope.deleteListing = function(index) {
+      index = $scope.findItemIndex()
+      console.log("works", index);
+      $scope.listings.splice(index,1);
+    };
+    $scope.showDetails = function(index) {
+    };
+    //Returns item to print it's details in HTML
+    $scope.findItemIndex = function(){
+      for(let i = 0; i < $scope.listings.length; i++){
+          if($scope.listings[i].code == $scope.itemCode.value){
+            return i;
+          }
+      }
+    };
+    $scope.findItem = function(){
+      for(let i = 0; i < $scope.listings.length; i++){
+          if($scope.listings[i].code == $scope.itemCode.value){
+            return $scope.listings[i];
+          }
+      }
+    };
   }
 ]);
